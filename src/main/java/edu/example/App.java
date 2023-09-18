8389edc9ec7ddd97bdf6904e494997b473e42d90
m.deryugin <m.deryugin@tinkoff.ru>
2023-09-18 19:45:21 +0300

    wrapper + lombok

--- src/main/java/edu/example/App.java
+++ src/main/java/edu/example/App.java
@@ -6,8 +6,19 @@ package edu.example;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+
+        var person1 = new Person();
+        var person2 = new PersonLombok();
+
+        person1.setAge(1);
+        person2.setAge(1);
+
+        person1.setName("Maks");
+        person2.setName("Maks");
+
+        System.out.println(person1);
+        System.out.println(person2);
     }
 }
---
+++ src/main/java/edu/example/PersonLombok.java
@@ -0,0 +1,9 @@
+package edu.example;
+
+import lombok.Data;
+
+@Data
+public class PersonLombok {
+    private Integer age;
+    private String name;
+}
